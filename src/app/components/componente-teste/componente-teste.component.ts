c29c0a400fb4338c7fdd3ed8e03c2226bc26a576
Felipe <felipe.svfx@gmail.com>
2020-11-21 15:37:03 -0300

    create first component

---
+++ src/app/components/componente-teste/componente-teste.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-componente-teste',
+  templateUrl: './componente-teste.component.html',
+  styleUrls: [],
+})
+export class ComponenteTesteComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
