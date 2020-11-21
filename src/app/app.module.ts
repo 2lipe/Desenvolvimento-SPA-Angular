b0b9e48b323e24864517607d42ae5266d58c500b
Felipe <felipe.svfx@gmail.com>
2020-11-21 15:59:03 -0300

    create first service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ComponenteTesteComponent } from './components/componente-teste/componente-teste.component';
 import { HomeComponent } from './components/home/home.component';
+import { ServicoService } from './servico.service';
 
 @NgModule({
   declarations: [AppComponent, ComponenteTesteComponent, HomeComponent],
   imports: [BrowserModule, AppRoutingModule],
-  providers: [],
+  providers: [ServicoService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
---
+++ src/app/funcionalidade/funcionalidade.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  declarations: [
+    //Componentes
+  ],
+  imports: [
+    CommonModule,
+    //Modulos
+  ],
+  providers: [
+    //Serviços
+  ],
+})
+export class FuncionalidadeModule {}
